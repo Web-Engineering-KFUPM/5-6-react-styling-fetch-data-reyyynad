dfd2124f04aeac42041a4ad9e232e46019504fd6
reyyynad <s202276760@kfupm.edu.sa>
2025-10-15 11:28:55 +0300

    TODO2 DONE

--- 5-6-react-styling-fetch-data/src/App.jsx
+++ 5-6-react-styling-fetch-data/src/App.jsx
@@ -322,13 +322,52 @@ import UserList from './components/UserList'
 import SearchBar from './components/SearchBar'
 import UserModal from './components/UserModal'
 
+const [users, setUsers] = useState([]);
+const [filteredUsers, setFilteredUsers] = useState([]);
+const [loading, setLoading] = useState(true);
+const [error, setError] = useState(null);
+const [searchTerm, setSearchTerm] = useState('');
+const [showModal, setShowModal] = useState(false);
+const [selectedUser, setSelectedUser] = useState(null);
+
+
 function App() {
   const [users, setUsers] = useState([])
 
   useEffect(() => {
-    {/*API fetch logic*/}
+   const fetchUsers = async () =>{
+      setLoading(true);
+      try{
+         const response = await fetch('https://jsonplaceholder.typicode.com/users');
+         const data = await response.json();
+         setUsers(data);
+         setFilteredUsers(data);
+      }
 
-  }, [])
+      catch(err){
+         setError(err.message);
+      }
+
+      finally{
+         setLoading(false);
+      }
+   };
+   fetchUsers();
+    
+
+  }, []);
+
+  useEffect(() => {
+   if (!searchTerm) {
+     setFilteredUsers(users);
+   } else {
+     const filtered = users.filter(user =>
+       user.name.toLowerCase().includes(searchTerm.toLowerCase())
+     );
+     setFilteredUsers(filtered);
+   }
+ }, [searchTerm, users]);
+ 
 
   const handleUserClick = (user) => {
   }
